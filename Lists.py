af65c0777fa1475d08088723415c44b3a193886f
james <jamesmoon2@gmail.com>
2023-11-06 08:55:21 -0500

    New Project and Repo

--- Lists.py
+++ Lists.py
@@ -2,7 +2,44 @@
 
 my_list = ["Banana", "Cherry", "Apple"]
 
-print(mylist)
+print(my_list)
 
-my_list2 = list()
+item = my_list[0]
+print(item)
+
+for fruit in my_list:
+    print(fruit)
+
+if "Cherry" in my_list:  #search within a list
+    print("Yes")
+else:
+    print("No")
+
+print(my_list)
+
+print(len(my_list))  # calculate the number of entries in a list
+
+my_list.append("Lemon")  # add item to end of list
+
+print(my_list)
+
+my_list.insert(1, "Blueberry") #insert an item a specific index in the list
+
+print(my_list)
+
+my_list.pop() #returns the last entery and removes it
+
+print(my_list)
+
+my_list.remove("Banana")  #remove a specific item
+
+print(my_list)
+
+my_list.reverse() #reverses the order
+
+print(my_list)
+
+my_list.sort() #sorts alphabetically, permanently. You can also create a new list sorted to keep both
+
+print(my_list)
 
