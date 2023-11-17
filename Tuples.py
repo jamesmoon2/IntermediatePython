50f1c7d57563eb65bd99d66a51e1e0c7c92603fb
james <jamesmoon2@gmail.com>
2023-11-17 16:03:52 -0500

    New Project and Repo

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+# tuples: ordered, immutable, allows duplicate elements; must have a comma to be a tuple
+
+mytuple = ("Max", 28, "Boston")
+
+#you can create tuples from a list or other function by using the tuple function tuple()
+
+print(mytuple)
